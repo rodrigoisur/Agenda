714c228f11f3ecf092f548c74ecbdbe2313a8ea7
Rodrigo <r.cano.zereceda@isur.edu.pe>
2013-09-30 18:12:04 -0500

    Agenda
    agenda 1ra

---
+++ Agenda/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agenda
+{
+    class Persona
+    {
+        #region atributos
+
+        private string _Nombre;
+        private string _Apellidos;
+        private int  _Telefono;
+        private string _Direccion;
+        private DateTime _FechaNacimiento;
+
+
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return _Apellidos ; }
+            set { _Apellidos = value; }
+        }
+
+        public int Telefono
+        {
+            get { return _Telefono; }
+            set { _Telefono = value; }
+        }
+
+        public string  Direccion
+        {
+            get { return _Direccion; }
+            set { _Direccion = value; }
+        }
+
+        public DateTime  FechaNacimiento
+        {
+            get { return _FechaNacimiento; }
+            set { _FechaNacimiento = value; }
+        }
+        #endregion
+        #region constructor
+
+        public Persona ( string nomb, string ape, string dire, DateTime fechana, int telef)
+        
+        {
+
+            _Nombre  = nomb;
+            _Apellidos = ape;
+            _Direccion = dire;
+            _FechaNacimiento = fechana;
+            _Telefono = telef;
+
+        }
+            
+        #endregion
+
+
+    }
+}
